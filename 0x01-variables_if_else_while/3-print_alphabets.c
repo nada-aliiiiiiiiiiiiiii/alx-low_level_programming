1040ee4c67ad95d2b1b8301fa928ad74d55806af
nada-aliiiiiiiiiiiiiii <nada63958@gmail.com>
2023-03-17 14:59:06 -0700

    dfghjkl

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,8 +6,9 @@
 */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; int i;
-for(i = 0; i < 52; i++)
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+for (i = 0; i < 52; i++)
 {
 putchar(alp[i]);
 }
