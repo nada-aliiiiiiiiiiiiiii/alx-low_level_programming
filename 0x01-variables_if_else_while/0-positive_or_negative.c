da764998dd351e219c1e12b76c40768bf970e8ec
nada-aliiiiiiiiiiiiiii <nada63958@gmail.com>
2023-03-17 14:33:46 -0700

    dfghjkl

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
+
 /**
 * main - Determines if a number is positive, negative or zero.
-* Return: Always 0 (Success)
+* Return: Always 0
 */
+
 int main(void)
 {
 int n;
@@ -21,7 +24,7 @@ printf("%d is zero\n", n);
 else
 {
 printf("%d is negative\n", n);
-} 
+}
 return (0);
 
 }
