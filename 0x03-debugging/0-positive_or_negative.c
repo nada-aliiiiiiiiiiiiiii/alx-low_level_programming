d66bc467ddc9ddfaac937b439f39cc0f47d801ff
nada-aliiiiiiiiiiiiiii <nada63958@gmail.com>
2023-03-24 19:17:50 -0700

    fghjk

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,11 +1,6 @@
 #include "main.h"
 
-/**
-* main - Entry point
-* positive_or_negative - check for positive or negative numbers
-* @i: the number to be checked
-* Return: Always 0 (Success)
-*/
+
 void positive_or_negative(int i)
 {
 if (i > 0)
